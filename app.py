732000bf82bd34093255400ca42eaaa0dd036513
facecchiamarco <facecchiamarco@icloud.com>
2024-02-10 18:47:27 +0100

    Added test POST request from form to Python Script

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+#FIXME: Change handle URL with UID
+@app.route("/register/handle", methods = ['POST'])
+def handle_form_data():
+    firstName = request.form.get('fname')
+    lastName = request.form.get('lname')
+    email = request.form.get('email')
+    pw = request.form.get('password')
+    role = request.form.get('role')
+
+    return f"<p>{firstName}</p>, <p>{lastName}</p>, <p>{email}</p>, <p>{pw}</p>, <p>{role}</p>"
 
 
 if __name__ == "__main__":
