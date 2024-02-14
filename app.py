e96f47d0473d75c1cf0a733eda5b977d01166131
facecchiamarco <facecchiamarco@icloud.com>
2024-02-14 12:11:29 +0100

    Added data acquisition and encryption functionality

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, url_for, request, redirect
+from argon2 import PasswordHasher, exceptions
 
 roleOptions = ['Student', 'Teacher']
 
@@ -14,7 +15,24 @@ def register():
 
 @app.route('/request')
 def handle_request(methods = 'POST'):
-    pass
+    #TODO: Make async request
+    if(request.form.get('role') in roleOptions):
+        
+        pwHasher = PasswordHasher()
+
+        fname = request.form.get('fname')
+        lname = request.form.get('lname')
+        email = request.form.get('email')
+        pw = request.form.get('password')
+        role = request.form.get('role')
+
+        pw = pw.encode()
+        hashedPW = pwHasher.hash(pw)
+
+
+
+    else:
+        return(redirect('/register'))
 
 
 if __name__ == "__main__":
