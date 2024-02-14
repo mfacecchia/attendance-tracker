52a3d50c20c55c24bb5449b1b8552d8201c0de99
facecchiamarco <facecchiamarco@icloud.com>
2024-02-14 11:30:40 +0100

    Cleared python script and register page

--- app.py
+++ app.py
@@ -7,21 +7,6 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/register')
-def register():
-    return render_template('register.html')
-
-#FIXME: Change handle URL with UID
-@app.route("/register/handle", methods = ['POST'])
-def handle_form_data():
-    firstName = request.form.get('fname')
-    lastName = request.form.get('lname')
-    email = request.form.get('email')
-    pw = request.form.get('password')
-    role = request.form.get('role')
-
-    return f"<p>{firstName}</p>, <p>{lastName}</p>, <p>{email}</p>, <p>{pw}</p>, <p>{role}</p>"
-
 
 if __name__ == "__main__":
     app.run(debug = True)
