a86ace5ac59b23e926900c5021bf7b1bae2d902d
facecchiamarco <facecchiamarco@icloud.com>
2024-02-29 18:22:49 +0100

    Added message screening styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./static/script/*.js", "./templates/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        'hide': {
+          '0%': { opacity: '1' },
+          '99%': { visibility: 'hidden' },
+          '100%': { opacity: '0' }
+        }
+      },
+      animation: {
+        'hide': 'hide 200ms ease-in-out forwards 5s'
+      }
+    },
   },
   plugins: [],
 }
