0d3292e5231f3efb5823600a44f5a09a2aec0c62
facecchiamarco <facecchiamarco@icloud.com>
2024-03-15 18:52:17 +0100

    Updated pie chart styling

--- static/script/pie.js
+++ static/script/pie.js
@@ -4,12 +4,20 @@ const pieChartConfig = {
     options: {
         plugins: {
             legend: {
-                position: 'right'
+                position: 'right',
+                labels: {
+                    font: {
+                        size: 14
+                    },
+                }
             }
         },
+        aspectRatio: 1
     }
 };
 var pieChart = new Chart(pieCanvas, pieChartConfig);
+resizeChart();
+window.addEventListener('resize', resizeChart);
 
 function updatePieChart(apiData){
     //`percentages` variable represents the percentages calculated for the "attended/not attended" relation (rounded to 2 decimal signs)
@@ -36,4 +44,23 @@ function updatePieChart(apiData){
         }]
     }
     pieChart.update();
+}
+
+//Resiezes the pie chart based on the some set breakponts
+function resizeChart(){
+    //Matrix containing every breakpoint to check for and the relative scale to apply to the chart
+    let breakpoints = [[0, 1], [768, 1.4], [1024, 1.6], [1280, 1.8], [1536, 3], [9999]];
+    //`breakpoints.every()` stops whenever the executed function returns `false`, in order to not overwhelm the user CPU
+    breakpoints.every((breakpoint, index) => {
+        //Iterating through each breakpoint and comparing those values with the client's brower size
+        if(window.screen.width >= breakpoint[0] && window.screen.width < breakpoints[index + 1][0]){
+            //Applying the object defined value and updating the chartm then quitting the loop
+            pieChart.options.aspectRatio = breakpoint[1];
+            return false
+        }
+        else{
+            return true
+        }
+    });
+    pieChart.update()
 }
