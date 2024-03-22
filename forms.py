77bbb7b163fcbc7a909373ead920123af89dc1b4
facecchiamarco <facecchiamarco@icloud.com>
2024-03-22 20:26:53 +0100

    Added WTForms Login form

--- forms.py
+++ forms.py
@@ -2,3 +2,10 @@ from wtforms import Form, StringField, EmailField, PasswordField, BooleanField, 
 from wtforms.validators import InputRequired, Length
 
 defaultFormsClass = 'formInputBox'
+defaultButtonClass = 'button'
+
+class LoginForm(Form):
+    Email = EmailField(validators = [InputRequired()], render_kw = {'placeholder': 'Email', 'class': defaultFormsClass})
+    password = PasswordField(validators = [InputRequired()], render_kw = {'placeholder': 'Password', 'class': defaultFormsClass})
+    remember = BooleanField('Ricordami')
+    submitForm = SubmitField('Login', render_kw = {'class': defaultButtonClass})
