8e8c67f7173b37725f89c7c2fc6fe3d53ff1bcc9
facecchiamarco <facecchiamarco@icloud.com>
2024-03-22 20:25:20 +0100

    Added WTForms initial setup

---
+++ forms.py
@@ -0,0 +1,4 @@
+from wtforms import Form, StringField, EmailField, PasswordField, BooleanField, SelectField, SubmitField, validators
+from wtforms.validators import InputRequired, Length
+
+defaultFormsClass = 'formInputBox'
